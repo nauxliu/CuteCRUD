91b8224dd89ad3d691bb628761fa30c4ecdfb115
NauxLiu <nauxliu@gmail.com>
2015-05-14 22:49:19 +0800

    add app view and style.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>@yield('title', 'CuteCRUD')</title>
+
+    <!-- Application Core CSS -->
+    <link href="/build/css/app.css" rel="stylesheet">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+@include('layouts.nav')
+
+<div id="wrapper">
+
+    @include('layouts.sidebar')
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+        @yield('content', 'Hello, CuteCRUD')
+    </div>
+    <!-- /#page-content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- Application Core JavaScript -->
+<script src="/build/js/app.js"></script>
+
+<!-- Menu Toggle Script -->
+<script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
+</body>
+</html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,13 @@
+
+<!-- Sidebar -->
+<div id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+            <a href="#">Models</a>
+        </li>
+        @foreach($models as $model)
+        <li><a href="#">{{$model->model_name}}</a></li>
+        @endforeach
+    </ul>
+</div>
+<!-- /#sidebar-wrapper -->
