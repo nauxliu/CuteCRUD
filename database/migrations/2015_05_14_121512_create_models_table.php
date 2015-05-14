8b5b27c83a18caeec3d89c956c3b32e057165fe9
NauxLiu <nauxliu@gmail.com>
2015-05-14 20:56:23 +0800

    add model's migration.

---
+++ database/migrations/2015_05_14_121512_create_models_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelsTable extends Migration {
+    private $table_name;
+
+    function __construct()
+    {
+        $this->table_name = env('TABLE_PREFIX') . 'models';
+    }
+
+    /**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create($this->table_name, function(Blueprint $table){
+            $table->increments('id');
+            $table->string('model_name', 10);
+            $table->string('table_name', 100);
+            $table->string('creatable', 100);
+            $table->string('editable', 100);
+            $table->string('listable', 100);
+            $table->timestamps();
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+        Schema::drop($this->table_name);
+	}
+
+}
